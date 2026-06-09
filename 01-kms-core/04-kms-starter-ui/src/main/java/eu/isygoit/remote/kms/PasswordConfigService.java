79bd22397a182be6eab7782703ed39de03c3651c
SMB <sami.mbarki@gmail.com>
2026-06-09 11:41:19 +0100

    Add random key management

---
+++ 01-kms-core/04-kms-starter-ui/src/main/java/eu/isygoit/remote/kms/PasswordConfigService.java
@@ -0,0 +1,15 @@
+package eu.isygoit.remote.kms;
+
+import eu.isygoit.com.rest.api.IMappedCrudApi;
+import eu.isygoit.config.FeignConfig;
+import eu.isygoit.dto.data.DigestConfigDto;
+import eu.isygoit.dto.data.PasswordConfigDto;
+import org.springframework.cloud.openfeign.FeignClient;
+
+/**
+ * The interface Kms incremental key service.
+ */
+@FeignClient(configuration = FeignConfig.class, name = "key-service", contextId = "password-config", path = "/api/v1/private/config/password")
+public interface PasswordConfigService extends IMappedCrudApi<Long, PasswordConfigDto, PasswordConfigDto> {
+
+}
