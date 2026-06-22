bf48c1262b01f2dc1296f9b366a67f32670ed128
SMB <sami.mbarki@gmail.com>
2026-06-22 16:54:29 +0100

    fix navigation issue

--- 01-kms-core/04-kms-starter-ui/src/main/java/eu/isygoit/ui/common/view/ManagementVerticalView.java
+++ 01-kms-core/04-kms-starter-ui/src/main/java/eu/isygoit/ui/common/view/ManagementVerticalView.java
@@ -2,8 +2,6 @@ package eu.isygoit.ui.common.view;
 
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.router.AfterNavigationEvent;
-import com.vaadin.flow.router.AfterNavigationObserver;
 import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.BeforeEnterObserver;
 import eu.isygoit.util.SecurityUtils;
